51c85049da10b9f04107f2d9e4f58c4003f2004a
atrDaw <161941079+atrDaw@users.noreply.github.com>
2026-06-19 17:55:58 +0200

    Add Hola Mundo user script for console logging

---
+++ mono.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Hola Mundo - Prueba
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Script de prueba que hace console.log en todas las páginas
+// @author       Tu Nombre
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/TUUSUARIO/TUREPO/main/hola-mundo.user.js
+// @downloadURL  https://raw.githubusercontent.com/TUUSUARIO/TUREPO/main/hola-mundo.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hola mundo');
+})();
