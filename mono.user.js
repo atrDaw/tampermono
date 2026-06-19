d20863915665d1b7604f3354b5e8d6498fcee321
atrDaw <161941079+atrDaw@users.noreply.github.com>
2026-06-19 18:43:34 +0200

    Update mono.user.js

--- mono.user.js
+++ mono.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Hola Mundo - Prueba
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Script de prueba que hace console.log en todas las páginas
 // @author       Tu Nombre
 // @match        *://*/*
-// @updateURL    https://github.com/atrDaw/tampermono/raw/refs/heads/main/mono.user.js
-// @downloadURL  https://github.com/atrDaw/tampermono/raw/refs/heads/main/mono.user.js
+// @updateURL    https://raw.githubusercontent.com/atrDaw/tampermono/main/mono.user.js
+// @downloadURL  https://raw.githubusercontent.com/atrDaw/tampermono/main/mono.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -14,7 +14,5 @@
     'use strict';
 
     console.log('hola mundo');
-    console.log(4+2);
-    console.log('nueva actualizacion 1.3')
-    
+    console.log('hola mundo');
 })();
